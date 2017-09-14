21036c3e5ec5a22f5c4979e33ec2d1958db82644
Julie.Canevet <julie.canevet@hotmail.fr>
2017-09-14 17:00:40 +0200

    add form commentaire and articles

--- commentaires.php
+++ commentaires.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Article / commentaires</title>
+        <link rel = "stylesheet" href = "font-awesome-4.7.0/css/font-awesome.min.css"> 
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
 <?php
 
 try //Connexion a la base de donnees
@@ -10,6 +21,21 @@ catch(Exception $e)
 }
 
 
+
+// AJOUT D'UN COMMENTAIRE
+// on verifie que les champs soient remplis et on les securise
+if(!empty($_POST['auteur']) && !empty($_POST['commentaire'])){
+
+	$auteur = htmlspecialchars($_POST['auteur']);
+	$com = htmlspecialchars($_POST['commentaire']);
+
+	$req = $bdd -> prepare('INSERT INTO commentaires (id_billet, auteur, commentaire) VALUES (:id_billet, :auteur, :commentaire)');
+	$req -> execute(array('id_billet' => $_GET['valId'],
+							'auteur' => $_POST['auteur'],
+							'commentaire' => $_POST['commentaire']
+		));
+}
+
 // verifie que les donnees existent
 if ($_GET['valId']){
 
@@ -18,17 +44,37 @@ if ($_GET['valId']){
 
 		$valId = htmlspecialchars($_GET['valId']);
 
+		// prepare l'article cible et execute
+		$article = $bdd -> prepare('SELECT * FROM billets WHERE id=:id');
+		$article -> execute(array('id' => $valId));
+		
 		//prepare les commentaires et execute
-		$commentaires = $bdd ->prepare('SELECT * FROM commentaires WHERE id_billet=:id');
+		$commentaires = $bdd ->prepare('SELECT auteur, DATE_FORMAT(date_commentaire, \'%Hh%i le %d/%m/%Y\') AS date_commentaire, commentaire FROM commentaires WHERE id_billet=:id ORDER BY date_commentaire');
 		$commentaires -> execute(array('id' => $valId));
-
-		foreach ($commentaires as $key => $value) {
-			echo $value['auteur'];?><br><?php
-			echo $value['commentaire'];?><br><?php
+		
+		//affiche l'article cible
+		foreach ($article as $clef => $valeur) {?><div class="article"><h3><?php
+			echo $valeur['titre'];?></h3><?php
+			echo $valeur['contenu'];?><br></div><br><?php
 		}
+		//affiche les commentaires cibles
+		foreach ($commentaires as $key => $value) {?><div class="com"><?php
+			echo $value['auteur'] . ' : (à ' . $value['date_commentaire'] . ')'?><br><?php
+			echo $value['commentaire'];?></div><br><?php
+		}?><br><a href="index.php" class="retour">Retour</a><?php
 	}	
 }	
 	else {
 			echo 'Aucun article selectionné';
 	}
 ?>
+
+<!-- creation du formulaire pour commentaires -->
+<form method="post" action="commentaires.php?valId=<?= $_GET['valId'];?>">
+	<label>Auteur : </label><input type="varchar" name="auteur">
+	<label>Commentaire : </label><input type="text" name="commentaire">
+	<input type="submit">
+</form>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Article / commentaires</title>
+        <link rel = "stylesheet" href = "font-awesome-4.7.0/css/font-awesome.min.css"> 
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
 <?php
 
 try //Connexion a la base de donnees
@@ -7,6 +18,24 @@ try //Connexion a la base de donnees
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
+}
+?>
+
+<form method="post" action="index.php">
+	<label>Titre : </label><input type="varchar" name="titre">
+	<label>Article : </label><input type="text" name="contenu">
+	<input type="submit">
+</form>
+<?php
+if(!empty($_POST['titre']) && !empty($_POST['contenu'])){
+
+$titre = htmlspecialchars($_POST['titre']);
+$contenu = htmlspecialchars($_POST['contenu']);
+
+	$req = $bdd -> prepare('INSERT INTO billets (id, titre, contenu, date_creation) VALUES ("", :titre, :contenu, "")');
+	$req -> execute(array('titre' => $titre,
+							'contenu' => $contenu
+		));
 }
 
 // On récupere les 5 derniers articles
@@ -20,5 +49,6 @@ $articles = $bdd -> query('SELECT * FROM billets ORDER BY id DESC LIMIT 0, 5');
 		<a href="commentaires.php?valId=<?php echo $value['id']; ?>">Commentaires</a><br><br>
 		<?php 
 	}
-
 ?>
+   </body>
+</html>
